9e2426b1eba35a63bffe6176af38e5d01235a2ea
翔 玉河 <210306@jc-21.jp>
2023-01-22 16:01:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Engine/Camera.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "Direct3D.h"
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+//-----------------------------------------------------------
+//カメラ
+//-----------------------------------------------------------
+namespace Camera
+{
+	//初期化（プロジェクション行列作成）
+	void Initialize(int winW, int winH);
+
+	//更新（ビュー行列作成）
+	void Update();
+
+	//視点（カメラの位置）を設定
+	void SetPosition(XMVECTOR position);
+	void SetPosition(XMFLOAT3 position);
+
+	//焦点（見る位置）を設定
+	void SetTarget(XMVECTOR target);
+
+	//ビュー行列を取得
+	XMMATRIX GetViewMatrix();
+
+	//プロジェクション行列を取得
+	XMMATRIX GetProjectionMatrix();
+
+	XMFLOAT3 GetCameraPosition();
+	XMFLOAT3 GetCameraTarget();
+	XMVECTOR GetCameraVecPosition();
+	XMVECTOR GetCameraVecTarget();
+
+	//カメラの視線ベクトルを取得
+	XMVECTOR GetSight();
+
+	//プロジェクション行列をセット
+	void SetProjMatrix(int winW, int winH, int split);
+};
---
+++ Engine/Model.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Fbx.h"
+#include "Transform.h"
+
+struct Fileset
+{
+	Fbx* pFbx = nullptr;		//Fbxのポインタ
+	Transform transform;		//transformクラス
+	std::string FileName;		//ファイルの名前
+	bool FindFbx = false;		//Fbxファイルを事前にロードしているか
+};
+
+namespace Model
+{
+	int Load(std::string filename);
+	void SetTransform(int hModel, Transform transform);
+	void Draw(int hModel, char SHADER_TYPE = 1);
+	void Draw(int hModel, Transform transformchar, char SHADER_TYPE = 1);
+	void Draw(int hModel, XMFLOAT3 Chroma, float Bright, float Alpha, char SHADER_TYPE = 1);
+
+	void Release();
+
+	void RayCast(int hModel, RayCastData& Raydata);
+}
