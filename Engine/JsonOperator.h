3b2743aed887b414bae93ab0ccfd66439add5ebe
翔 玉河 <210306@jc-21.jp>
2023-06-12 11:30:17 +0900

    こめんとッ

--- Engine/JsonOperator.h
+++ Engine/JsonOperator.h
@@ -37,6 +37,14 @@ namespace JsonOperator
 		return Spare;
 	}
 
+	/// <summary>jsonファイルから値を取得する</summary>
+	/// <typeparam name="T">戻り値の型</typeparam>
+	/// <typeparam name="...Args">キー名のパラメータパック</typeparam>
+	/// <param name="data">読み込む対象のjsonオブジェクト</param>
+	/// <param name="Spare">読み込めなかった際の値</param>
+	/// <param name="key">キー名</param>
+	/// <param name="...args">キー名</param>
+	/// <returns>成功 : 読み込んだ値  失敗 : Spare</returns>
 	template<typename T, typename ... Args>
 	T GetData(const nlohmann::json& data, const T& Spare, const std::string& key, const Args& ... args)
 	{
@@ -48,6 +56,14 @@ namespace JsonOperator
 		return Spare;
 	}
 
+	/// <summary>ファイル名から値を取得する</summary>
+	/// <typeparam name="T">戻り値の型</typeparam>
+	/// <typeparam name="...Args">キー名のパラメータパック</typeparam>
+	/// <param name="filename">読み込む対象のファイル名</param>
+	/// <param name="Spare">読み込めなかった際の値</param>
+	/// <param name="key">キー名</param>
+	/// <param name="...args">キー名</param>
+	/// <returns>成功 : 読み込んだ値  失敗 : Spare</returns>
 	template<typename T, typename ... Args>
 	T GetData(const std::string& filename, const T& Spare, const std::string& key, const Args& ... args)
 	{
