ea1b1840112f61608d4d2c28e309de9484330ed4
翔 玉河 <210306@jc-21.jp>
2023-06-15 14:50:46 +0900

    リニューアル

--- Engine/Time.cpp
+++ Engine/Time.cpp
@@ -1,5 +1,6 @@
 #include "Time.h"
 #include <chrono>
+#include <list>
 
 //変数
 namespace
@@ -9,6 +10,8 @@ namespace
 	std::chrono::steady_clock::time_point Prev = std::chrono::steady_clock::now();	//前フレームのシステム時間を保存
 	std::chrono::steady_clock::time_point Now = std::chrono::steady_clock::now();	//現在のシステム時間を保存
 	std::chrono::milliseconds MilliSeconds_ = std::chrono::milliseconds::zero();	//経過時間
+
+	std::list<Time::Watch*> WatchList = {};	//生成されたWatchクラスを管理しておく配列
 }
 
 namespace Time
@@ -18,6 +21,18 @@ namespace Time
 		//2つのシステム時間を更新
 		Prev = Now;
 		Now = std::chrono::steady_clock::now();
+
+		if(!WatchList.empty())
+		for (auto&& itr : WatchList)
+		{
+			if (!Lock_)
+			{
+				if(!itr->IsCountDown())
+					itr->MilliSeconds_ += (int)std::chrono::duration_cast<std::chrono::milliseconds>(Now - Prev).count();
+				else
+					itr->MilliSeconds_ -= (int)std::chrono::duration_cast<std::chrono::milliseconds>(Now - Prev).count();
+			}
+		}
 
 		//Lockされていたら更新しない
 		if (Lock_)
@@ -54,5 +69,58 @@ namespace Time
 	int GetHours()
 	{
 		return (int)std::chrono::duration_cast<std::chrono::hours>(MilliSeconds_).count();
+	}
+	
+
+	Watch::Watch() : Lock_(false), CountDown_(false), MilliSeconds_(0)
+	{
+		WatchList.push_back(this);
+	}
+
+	Watch::Watch(int sec) : Lock_(false), CountDown_(true), MilliSeconds_((int)std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(sec)).count())
+	{
+		WatchList.push_back(this);
+	}
+
+	Watch::~Watch()
+	{
+		//配列の中に存在する自身を検索し、削除する
+		if(auto itr = std::find(WatchList.begin(), WatchList.end(), this); itr != WatchList.end())
+			WatchList.erase(itr);
+	}
+
+	void Watch::Reset()
+	{
+		MilliSeconds_ = 0;
+	}
+
+	void Watch::Lock()
+	{
+		Lock_ = true;
+	}
+
+	void Watch::UnLock()
+	{
+		Lock_ = false;
+	}
+
+	int Watch::GetMilliSeconds()
+	{
+		return MilliSeconds_;
+	}
+
+	int Watch::GetMinutes()
+	{
+		return (int)std::chrono::duration_cast<std::chrono::minutes>(std::chrono::milliseconds(MilliSeconds_)).count();
+	}
+
+	int Watch::GetHours()
+	{
+		return (int)std::chrono::duration_cast<std::chrono::hours>(std::chrono::milliseconds(MilliSeconds_)).count();
+	}
+
+	bool Watch::IsCountDown()
+	{
+		return CountDown_;
 	}
 }
--- Engine/Time.h
+++ Engine/Time.h
@@ -1,10 +1,46 @@
 #pragma once
 
-/// <summary>
-/// DirectX環境に依存しないTime
-/// </summary>
+//ストップウォッチのような機能を持つ名前空間
 namespace Time
 {
+	//Timeと同じように個別で使用可能なクラス
+	class Watch
+	{
+		bool Lock_;		//現在の状態で固定するか
+		bool CountDown_;	//カウントダウンかカウントアップか
+
+	public:
+		int MilliSeconds_;	//経過時間
+
+		Watch();
+		Watch(int sec);
+		~Watch();
+
+		//時間を初期化
+		void Reset();
+
+		//時間の更新を一時停止
+		void Lock();
+
+		//時間の更新を再開
+		void UnLock();
+
+		//呼び出し時点の時間を取得
+		int GetMilliSeconds();
+
+		template<typename T = int>
+		T GetSeconds()
+		{
+			return (T)GetMilliSeconds() / 1'000;
+		}
+
+		int GetMinutes();
+		int GetHours();
+
+		//カウントダウン状態かどうか
+		bool IsCountDown();
+	};	//class Watch
+
 	//更新(Mainで呼び出し)
 	void Update();
 
